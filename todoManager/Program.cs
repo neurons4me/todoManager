6486cc1ca18568bc5f124e397658779ee422723b
Adam McDaniel <adammcdaniel1@gmail.com>
2019-09-27 10:36:49 -0700

    initial upload

---
+++ todoManager/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace todoManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> validBaseCommands = new List<String>() { "addlist", "additem", "rmlist", "rmitem", "lslist", "lsitem", "ls" };
+
+            if ( !(validBaseCommands.Contains(args[0])))
+            {
+                String listName = args[1];
+
+                switch (args[0])
+                {
+                    case "addlist":
+                        // connect to db
+                        // insert new list with listName
+                        break;
+
+                    case "additem":
+                       // connect to db
+                       // find listID beloning to listName
+                       // insert new record with args[2] as todoItemDescription and the listID from previous step as parentListID, auto assign other values
+                        break;
+
+                    case "rmlist":
+                        // connect to db
+                        //
+                        break;
+
+                    case "rmitem":
+                        
+                        break;
+
+                    case "lslist":
+                        
+                        break;
+
+                    case "lsitem":
+                        
+                        break;
+
+                    case "ls":
+                        //
+                        // console printout of all list names
+                        break;
+
+                    default:
+                        break;
+                }
+
+
+            } //if ( !(validBaseCommands.Contains(args[0])))
+            else
+            {
+                Console.WriteLine("INVALID ARGUMENT");
+            }
+           
+
+            
+            
+
+            // for item in todo list write line
+
+        }
+    }
+}
---
+++ todoManager/Todo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace todoManager
+{
+    public class clsTodoItem
+    {
+        public String itemDescription { get; set; }
+        public Int16 itemID { get; set; }
+        public DateTime creationTime { get; set; }
+        public DateTime completionTime { get; set; }
+        public Int16 isActive { get; set; }
+        public Int16 parentListID { get; set; }
+
+
+    }
+
+    public class clsTodoList
+    {
+        //it should work by running a query and pulling list items from db then populating this list
+        //public clsTodoItem[] listContents {get; set;}
+
+        //public clsTodoItem[] populateTodoList(String pListID)
+        //{
+        //    // connect to db
+        //    // run query for all items for listID
+        //    // populate each item with values
+        //    // append each item to the list
+        //    // return list of todoItem objects
+        
+        //}
+
+        public Int16 listID { get; set; }
+        public clsTodoItem[] todoListContents { get; set; }
+
+
+
+
+    }
+}
