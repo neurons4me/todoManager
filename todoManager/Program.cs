628a17fb544b3c0df1c36cb515987825a87389f7
Adam McDaniel <Adam.McDaniel@axiomglobal.com>
2019-09-27 14:00:06 -0700

    added lots of stuff

--- todoManager/Program.cs
+++ todoManager/Program.cs
@@ -7,45 +7,59 @@ namespace todoManager
     {
         static void Main(string[] args)
         {
-            List<String> validBaseCommands = new List<String>() { "addlist", "additem", "rmlist", "rmitem", "lslist", "lsitem", "ls" };
 
-            if ( !(validBaseCommands.Contains(args[0])))
+            List<String> validBaseCommands = new List<String>() { "addlist", "additem", "rmlist", "rmitem", "lslist", "lsitem", "ls", "done" };
+
+            if (String.IsNullOrEmpty(args[0]))
+	        {
+                Console.WriteLine("NO ARGUMENTS PROVIDED");
+
+	        }
+
+        
+
+            else if ( (validBaseCommands.Contains(args[0])))
             {
-                String listName = args[1];
+                //String listName = args[1];
 
                 switch (args[0])
                 {
                     case "addlist":
                         // connect to db
+                        dbUtility.addListItemToDB("Server=LAPTOP-PMN2DM89;Database=db_todoApp;User Id=AppAccess;Password=dmcasucks;");
+
                         // insert new list with listName
                         break;
 
-                    case "additem":
+                    case "additem": // adds a list item to a given list
                        // connect to db
                        // find listID beloning to listName
                        // insert new record with args[2] as todoItemDescription and the listID from previous step as parentListID, auto assign other values
                         break;
 
-                    case "rmlist":
+                    case "rmlist": // removes an entire list and it's contents
                         // connect to db
                         //
                         break;
 
-                    case "rmitem":
+                    case "rmitem": //marks an item as removed
                         
                         break;
 
-                    case "lslist":
+                    case "lslist": // fetches a given list's items, sorts by time created, applies numbers to them and displays a numbered list; prompts for input for add, remove, done
                         
                         break;
 
-                    case "lsitem":
+                    case "lsitem": //lists all items in a given list
                         
                         break;
 
-                    case "ls":
-                        //
-                        // console printout of all list names
+                    case "ls": //lists all lists by name
+
+                        break;
+
+                    case "done": //lists all lists by name
+
                         break;
 
                     default:
---
+++ todoManager/dbUtility.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace todoManager
+{
+    public class dbUtility
+    {
+        public static void addListItemToDB(String pConnectionString)
+        {
+            using (SqlConnection connection = new SqlConnection(pConnectionString))
+            {
+                connection.Open();
+                // Do work here; connection closed on following line.
+
+                connection.Close();
+            }
+
+        }
+
+    }
+}
